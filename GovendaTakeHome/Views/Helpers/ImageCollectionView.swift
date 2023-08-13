bef70fb59d40d47bd1598b94464b34001e91260a
Zac Duvall <duvallzac@gmail.com>
2023-08-13 09:43:24 -0600

    - implement caching in ImageCollectionView

--- GovendaTakeHome/Views/Helpers/ImageCollectionView.swift
+++ GovendaTakeHome/Views/Helpers/ImageCollectionView.swift
@@ -7,15 +7,20 @@
 
 import UIKit
 
-/// `ImageCollectionView` is a helper class that implements the `UICollectionViewDataSource` and `UICollectionViewDelegate` protocols.
-/// It manages the data source and delegate methods for a collection view that displays images.
+/// `ImageCollectionView` is a helper class that manages the data source and delegate methods
+/// for a collection view that displays images. It also includes caching to efficiently handle
+/// image loading.
 class ImageCollectionView: NSObject, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    /// An array of `UnsplashImage` objects representing the images to be displayed in the collection view.
+    /// An array of objects conforming to `ImageRepresentable` that represents the images to be displayed in the collection view.
     var images: [ImageRepresentable] = []
     
     /// A weak reference to the delegate of the `ImageCollectionView`. The delegate is responsible for fetching the images.
     weak var delegate: ImageCollectionViewDelegate?
+    
+    /// A cache for storing downloaded images, keyed by their URL as a string.
+    /// This cache helps in reusing downloaded images and reduces redundant network calls.
+    let imageCache = NSCache<NSString, UIImage>()
 
     /// Returns the number of items in the given section of the collection view.
     /// - Parameters:
@@ -26,45 +31,51 @@ class ImageCollectionView: NSObject, UICollectionViewDataSource, UICollectionVie
         return images.count
     }
 
-    /// Asks your data source object for the cell that corresponds to the specified item in the collection view.
+    /// Asks the data source for the cell that corresponds to the specified item in the collection view.
+    /// It also handles image caching and asynchronous loading.
     /// - Parameters:
     ///   - collectionView: The collection view requesting this information.
     ///   - indexPath: The index path that specifies the location of the item.
-    /// - Returns: A configured cell object. You must not return `nil` from this method.
+    /// - Returns: A configured cell object with the appropriate image.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.reuseId, for: indexPath) as? ImageCollectionViewCell else { return UICollectionViewCell() }
         let image = images[indexPath.row]
         
-        // Set a placeholder image or clear the current image
+        // Check if the image is already cached, and use it if available
+        if let cachedImage = imageCache.object(forKey: image.imageUrl.absoluteString as NSString) {
+            cell.configure(with: cachedImage)
+            return cell
+        }
+        
+        // Set a placeholder image or clear the current image while the actual image is being fetched
         cell.configure(with: nil)
         
-        delegate?.fetchImage(for: image.imageUrl) { result in
+        // Fetch the image through the delegate, cache it, and update the cell
+        delegate?.fetchImage(for: image.imageUrl) { [weak self] result in
             switch result {
-            case .success(let image):
-                cell.configure(with: image)
+            case .success(let resultImage):
+                // Cache the downloaded image
+                self?.imageCache.setObject(resultImage, forKey: image.imageUrl.absoluteString as NSString)
+                // Configure the cell with the downloaded image
+                cell.configure(with: resultImage)
             case .failure(let error):
-                print("Error loading image: \(error)")
+                print("Error loading image: \(error)") // Consider handling this error more gracefully
             }
         }
         return cell
     }
     
+    /// Handles the selection of an item in the collection view and forwards the event to the delegate.
+    /// - Parameters:
+    ///   - collectionView: The collection view where the selection occurred.
+    ///   - indexPath: The index path of the selected item.
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         delegate?.collectionView(collectionView, didSelectImageAt: indexPath)
     }
     
     /// Updates the images to be displayed in the collection view.
-    /// - Parameter newImages: An array of `UnsplashImage` objects representing the new images.
+    /// - Parameter newImages: An array of objects conforming to `ImageRepresentable` representing the new images.
     func updateImages(_ newImages: [ImageRepresentable]) {
         self.images = newImages
     }
 }
-
-
-
-
-
-
-
-
-
